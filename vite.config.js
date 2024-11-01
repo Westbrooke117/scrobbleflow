2d54bf0ab742be0d148684c92f0d69636d5f37fc
kyhaa <kyhaanderson@gmail.com>
2024-11-01 14:04:11 +1000

    Added vite config base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://westbrooke117.github.io/scrobbleflow/",
   plugins: [react()],
 })
